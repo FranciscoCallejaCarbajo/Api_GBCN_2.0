e14f4f371b41b3a227db4974491f78ab5b8605af
Francisco <160488986+FranciscoCallejaCarbajo@users.noreply.github.com>
2024-07-02 10:44:20 +0200

    update pegamos todas las carpetas

---
+++ js/menuDesplegable.js
@@ -0,0 +1,8 @@
+let menuDesplegable = document.getElementById("menuDesplegable");
+
+function openmenu(){
+    menuDesplegable.style.right = "0";
+}
+function closemenu(){
+    menuDesplegable.style.right = "-250px";
+}
